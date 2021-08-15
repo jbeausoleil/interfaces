f1db62892383f6c6a65b6f96d362e12b8b2b7e57
Justin Beausoleil <beausoleil.justin@gmail.com>
2021-08-15 11:19:24 -0600

    Adding interface for bot.

--- main.go
+++ main.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
+type bot interface { // any type in the program that calls getGreeting and returns a string is of type bot
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
+	eb := englishBot{} // also of type bot
+	sb := spanishBot{} // also of type bot
 
 	printGreeting(eb)
 	printGreeting(sb)
 }
 
-func printGreeting (eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreeting (sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func printGreeting (b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (eb englishBot) getGreeting() string {
